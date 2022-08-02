89d4d8ad932015c8cf102e8bbdd446f3529c881c
Nurbek <n.nazarbay@mediana.kz>
2022-08-02 17:18:29 +0500

    upd

---
+++ app/Http/Controllers/TabelController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\TabelRepository;
+use Illuminate\Http\Request;
+
+class TabelController extends Controller
+{
+    protected $repository;
+
+    public function __construct(TabelRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+
+    public function chetvertTabel($locale, $id_predmet) {
+        $user = auth()->user();
+        $this->repository->init((int) $user->id_mektep);
+
+        $tabel = $this->repository->chetvertTabel($id_predmet, $user->id);
+
+        return response()->json($tabel, 200);
+    }
+}
