9603631ae4e72471dc3413b23f0b1ff247b214de
Nurbek <n.nazarbay@mediana.kz>
2022-09-05 16:12:21 +0500

    upd

--- config/mail.php
+++ config/mail.php
@@ -37,7 +37,7 @@ return [
         'smtp' => [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'mektep.edu.kz'),
-            'port' => env('MAIL_PORT', 25),
+            'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('robot@mektep.edu.kz'),
             'password' => env('U{i1+3o9,NV9'),
