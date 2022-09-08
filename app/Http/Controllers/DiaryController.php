f466bbe6d89837fd26336c3ddc751139e6aa658a
Nurbek <n.nazarbay@mediana.kz>
2022-09-08 15:47:02 +0500

    upd

--- app/Http/Controllers/DiaryController.php
+++ app/Http/Controllers/DiaryController.php
@@ -30,7 +30,9 @@ class DiaryController extends Controller
         $week += 13;
 
         $monday = date("Y-m-d", strtotime('monday '.$week.' week'));
-        $saturday = date_add($monday, date_interval_create_from_date_string("6 day"));
+        $saturday = $monday;
+        date_add($saturday, date_interval_create_from_date_string("6 day"));
+
         $diary = $this->repository->diary($monday, $saturday);
 
         $monday = date("d.m", strtotime($monday));
