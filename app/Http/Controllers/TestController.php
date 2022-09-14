a9ddcfd2fe7371c17067a2bf9a8a9e96896526ad
Nurbek <n.nazarbay@mediana.kz>
2022-09-14 10:38:56 +0500

    upd

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,8 +8,9 @@ use Illuminate\Support\Facades\Redis;
 class TestController extends Controller
 {
     public function test() {
-        Redis::set('name', 'alan');
+        Redis::set('teacher_token:42', 'asd5f4as621va8df5sssssssssss');
+        Redis::set('teacher_token:45', 'dddddddddddddddddddddddddddd');
 
-        return Redis::get('name');
+        return Redis::get('teacher_token:42');
     }
 }
